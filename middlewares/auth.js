b335f64d317f06342673b4ece50123a78399dcaf
marco <marcoberutti90@gmail.com>
2025-02-02 17:37:31 +0100

    fixed dotenv in dev

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,13 @@
 if (process.env.ENV !== "production") {
-  require("dotenv").config();
+  require("dotenv").config({ path: "./.env.local", debug: true });
 }
+
 
 const apiKeyMiddleware = (req, res, next) => {
   const apiKey = req.headers["x-api-key"];
 
   if (!apiKey || apiKey !== process.env.API_KEY) {
-    return res.status(403).json({ error: "Accesso negato. API Key non valida." });
+    return res.status(403).json({ error: `Accesso negato. API Key non valida.` });
   }
 
   next();
