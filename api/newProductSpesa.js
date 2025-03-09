920f4c716191bdc89d4454ecfe5f469bf456f395
marco <marcoberutti90@gmail.com>
2025-03-09 12:50:23 +0100

    more detailed errors in res

--- api/newProductSpesa.js
+++ api/newProductSpesa.js
@@ -39,7 +39,7 @@ router.post('/', async (req, res) => {
 
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Errore nella query' });
+    res.status(500).json({ error: 'Errore nella query' + err });
   }
 });
 
